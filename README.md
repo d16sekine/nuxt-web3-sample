109996da22e207c17520f78bf327e8c280a036a4
d16sekine <d16sekine@gmail.com>
2018-12-01 01:16:59 +0900

    add contents

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# nuxt-web3-sample
-Nuxt.js2.0からweb3.js 1.0を用いてgethにアクセスするコンテンツ
+# xds-app
+
+> Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
+
---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        xds-app
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+      
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import Web3 from 'web3'
+
+export default {
+  components: {
+    AppLogo
+  },
+  async mounted(){
+
+    const tokenAddress = "0x6dc9d424b5514f249c73093295917440a1614474";
+
+    const MyToken = await fetch("./assets/token/MyToken.json")
+    .then((res) => {
+        return res.json();
+    })
+
+    const web3 = new Web3('http://localhost:8545');
+    let myContract =  await new web3.eth.Contract(MyToken.abi, tokenAddress);
+    let totalSupply = await myContract.methods.totalSupply().call();
+    console.log("totalSupply:", totalSupply);
+
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
